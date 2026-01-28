f1c2513f3b8eb00e3825189e736bfc70ed257651
bill_glb <bill_glb@com>
2026-01-28 12:28:35 +0800

    jan 28

--- round/command.sh
+++ round/command.sh
@@ -3,12 +3,13 @@ set -e
 
 python bk_day.py
 
+python bk_pipeline_data_con.py
+python bk_pipeline_top_con.py
+python bk_pipeline_vis_con.py
+
 python bk_pipeline_data.py
 python bk_pipeline_top.py
 python bk_pipeline_vis.py
 
-python bk_pipeline_data_con.py
-python bk_pipeline_top_con.py
-python bk_pipeline_vis_con.py
 
 # echo "success"
