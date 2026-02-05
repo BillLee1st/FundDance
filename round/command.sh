0e0676e4e8118d0dae2c166c440cb646323966f9
bill_glb <bill_glb@com>
2026-02-05 19:58:17 +0800

    feb 5

--- round/command.sh
+++ round/command.sh
@@ -1,13 +1,13 @@
 # #!/bin/bash
 set -e
 
-python bk_day.py
+# python bk_day.py
 
-python bk_pipeline_data_con.py
+# python bk_pipeline_data_con.py
 python bk_pipeline_top_con.py
 python bk_pipeline_vis_con.py
 
-python bk_pipeline_data.py
+# python bk_pipeline_data.py
 python bk_pipeline_top.py
 python bk_pipeline_vis.py
 
